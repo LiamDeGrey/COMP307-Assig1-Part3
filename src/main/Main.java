3fb5cddedd3209c82b7270a3a1843b8bff69b642
LiamDeGrey <computernerddegrey@gmail.com>
2015-03-29 21:40:10 +1300

    added load file code to the program

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Created by Liam on 29-Mar-15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+
+        }
+    }
+}
